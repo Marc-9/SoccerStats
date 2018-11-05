be45098481cb535c1202dabc2ba40a50fa227029
Marc-9 <37602279+Marc-9@users.noreply.github.com>
2018-11-05 14:43:33 -0800

    Uploaded updated filed
    New update that coincides with readme

--- src/Season.java
+++ src/Season.java
@@ -66,13 +66,33 @@ public class Season{
 		
 		Season season_17_18 = new Season();
 		season_17_18.readGames("6.txt");
-		System.out.println(season_17_18);
+		
+		
+		double totalGoals = 0;
+		double totalShots = 0;
+		double shotsOnTarget = 0;
+		for(int i = 0; i < season_17_18.games.size(); i++) {
+			totalGoals += season_17_18.games.get(i).aGoals + season_17_18.games.get(i).hGoals;
+			totalShots += season_17_18.games.get(i).aShots + season_17_18.games.get(i).hShots;
+			shotsOnTarget += season_17_18.games.get(i).aShotsTarget + season_17_18.games.get(i).hShotsTarget;
+		}
+		double goalsPerGame = (totalGoals / season_17_18.games.size());
+		double shotsPerGoal = (totalShots/totalGoals);
+		double shotsonTperGoal = (shotsOnTarget/totalGoals);
+		System.out.printf("Total number of Goals scored in the 17/18 Season is %.0f\n", totalGoals);
+		System.out.printf("Total number of Shots in the 17/18 Season is %.0f\n", totalShots);
+		System.out.printf("Total number of Shots on target in the 17/18 Season is %.0f\n", shotsOnTarget);
+		System.out.printf("There were %.3f goals per game\n", goalsPerGame);
+		System.out.printf("There were %.3f shots per goal\n", shotsPerGoal);
+		System.out.printf("There were %.3f shots on target per goal\n", shotsonTperGoal);
+		
+		/*System.out.println(season_17_18);
 		
 		Date start = Games.createDate("19-08-17");
 		Date end = Games.createDate("23-12-17");
 		System.out.println("Start date: " + start.toString());
 		System.out.println("End date: " + end.toString());
-		System.out.println(season_17_18.findGames(start, end));
+		System.out.println(season_17_18.findGames(start, end));*/
 		
 		
 	
